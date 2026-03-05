a64a78359e010576ff2dcc0aaaf23baf5762d5cf
mahara28 <maha.riahi28@gmail.com>
2026-03-05 13:54:59 +0100

    corr

---
+++ src/app/app-shared/widgets/page-not-found/page-not-found.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mc-page-not-found',
+  imports: [],
+  templateUrl: './page-not-found.html',
+  styleUrl: './page-not-found.css',
+})
+export class PageNotFound implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
