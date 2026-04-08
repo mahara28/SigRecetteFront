386379edd32ece2956df13b3a73982ab69352429
Arij_Kahlaoui <arij.kahlaoui@mc.com.tn>
2026-04-08 11:13:23 +0100

    correction gestNomen

--- src/app/layouts/private/pages/parametrage-nomenclature/parametrage-nomenclature.uri.ts
+++ src/app/layouts/private/pages/parametrage-nomenclature/parametrage-nomenclature.uri.ts
@@ -1,5 +1,6 @@
 export const PARAM_NOMENCLATURE_URI = {
   NOMENCLATURE_LIST_DATA: "parametrageNomenclatures/data",
-  LIST: '/parametrageNomenclatures/data',
-  DATA: '/dynamic-nomenclature/data'
+  V_NOMENCLATURE_LIST_DATA: "parametrageNomenclatures/vdata",
+  LIST: 'parametrageNomenclatures/data',
+  DATA: 'dynamic-nomenclature/data'
 };
